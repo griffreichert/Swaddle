1bf70e9795f747cb1bc921ea752579552342b2ad
Griffin <you@example.com>
2020-09-25 17:31:31 -0400

    redux

--- obapp/App.js
+++ obapp/App.js
@@ -5,49 +5,51 @@ import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
   
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
 
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-  }
-  
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={ openImagePickerAsync }>
-        <Image
-          source={ require('./assets/camera.png') }
-          style={ styles.iconButton }
-        />
-      </TouchableOpacity>
-
-      <StatusBar style="auto" />
-    </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7760ea',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  iconButton: {
-    width: 40,
-    height: 40,
-    margin: 10,
-  },
-  text: {
+  // let openImagePickerAsync = async () => {
+  //   let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
 
-  },
-  title: {
+  //   if (permissionResult.granted === false) {
+  //     alert("Permission to access camera roll is required!");
+  //     return;
+  //   }
 
-  },
-});
+  //   let pickerResult = await ImagePicker.launchImageLibraryAsync();
+  //   console.log(pickerResult);
+  // }
+  
+  // return (
+  //   <View style={styles.container}>
+  //     <TouchableOpacity
+  //       onPress={ openImagePickerAsync }>
+  //       <Image
+  //         source={ require('./assets/camera.png') }
+  //         style={ styles.iconButton }
+  //       />
+  //     </TouchableOpacity>
+
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#7760ea',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   iconButton: {
+//     width: 40,
+//     height: 40,
+//     margin: 10,
+//   },
+//   text: {
+
+//   },
+//   title: {
+
+//   },
+// });

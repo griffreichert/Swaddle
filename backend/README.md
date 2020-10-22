8944c87f15f2a51017e6121d073d0402a0001232
ads221 <ads221@lehigh.edu>
2020-10-22 17:27:35 -0400

    added auth API

--- backend/README.md
+++ backend/README.md
@@ -2,3 +2,4 @@
 nodejs v8.10.0
 npm v3.5.2
 postgresql v10.14
+bcrypt v1.0.3
--- backend/queries.js
+++ backend/queries.js
@@ -1,24 +1,76 @@
 const Pool = require('pg').Pool
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 const pool = new Pool({
-  user: 'admin',
-  host: 'localhost',
-  database: 'notatechdatabase',
-  password: 'password',
-  port: 5432,
+	user: 'admin',
+	host: 'localhost',
+	database: 'notatechdatabase',
+	password: 'password',
+	port: 5432,
 })
 
 const create_user = (request, response) => {
-  const { email, first_name, last_name, password, phone } = request.body
+	const { email, first_name, last_name, password, phone } = request.body
 
-  pool.query('INSERT INTO users (email, first_name, last_name, password, phone) VALUES ($1, $2, $3, $4, $5)', [email, first_name, last_name, password, phone], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${result.insertId}`)
-  })
+	pool.query('INSERT INTO users (email, first_name, last_name, password, phone) VALUES ($1, $2, $3, $4, $5)', [email, first_name, last_name, hash_password(password), phone], (error, results) => {
+		if (error) { throw error }
+		response.status(201).send(`User added with ID: ${result.insertId}`)
+	})
+}
+
+const auth_user = (request, response) => {
+	const { email, password } = request.body
+
+	pool.query('SELECT password from users where email=$1', [email], (error, results) => {
+		if (error)
+		{
+			throw error
+		}
+		if (check_password(results.rows[0], hash_password(password)))
+		{
+			token = create_token()
+			pool.query('UPDATE users set token=$1 where email=$2', [token, email], (internal_error, internal_results) => { if (error) { throw error }})
+		}
+		else
+		{
+			response.status(401).send()
+		}
+		response.status(200).send(`${token}`)
+	})
 }
 
 module.exports = {
-  create_user
+	create_user
+	auth_user
 }
 
+const hash_password = (password) => {
+  return new Promise((resolve, reject) =>
+    bcrypt.hash(password, 10, (err, hash) => {
+      err ? reject(err) : resolve(hash)
+    })
+  )
+}
+
+const check_password = (req_password, hashed_password) => {
+  return new Promise((resolve, reject) =>
+    bcrypt.compare(req_password, hashed_password, (err, result) => {
+        if (err) {
+          reject(err)
+        }
+        else if (result) {
+          resolve(result)
+        } else {
+          reject(new Error('Passwords do not match.'))
+        }
+    })
+  )
+}
+
+const create_token = () => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(16, (err, data) => {
+      err ? reject(err) : resolve(data.toString('base64'))
+    })
+  })
+}

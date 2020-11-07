e0748893678bdeb3d8e8f2215e4c8caf20ad0bec
Griffin <you@example.com>
2020-11-07 17:57:26 -0500

    pull to refresh

--- obapp/components/Views/Feed.js
+++ obapp/components/Views/Feed.js
@@ -1,36 +1,52 @@
-import { isLoaded } from 'expo-font';
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, View } from 'react-native';
 import { Card, Chip, Text, Title, withTheme } from 'react-native-paper';
 import { connect } from 'react-redux'
 import Header from '../Header';
 import MediaButton from '../MediaButton';
+import axios from 'axios'
 
 class Feed extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            image: null,
-            tags: ['baby', 'cute', 'awwww', 'adorable'],
-            isLoaded: false,
+            posts: [
+                {
+                    id: 'item1',
+                    image: 'https://i.some-random-api.ml/Snzc4MEWcZ.jpg',
+                    tags: ['bark', 'play', 'cuddle'],
+                },
+                {
+                    id: 'item2',
+                    image: 'https://i.some-random-api.ml/cKawj2aC4O.jpg',
+                    tags: ['woof', 'ears', 'adorable'],
+                },
+            ],
+            isLoading: false,
         };
     }
+
     componentDidMount() {
-        this.loadPosts()
-        this._refresh = this.props.navigation.addListener(
-            'focus', () => {
-                console.log("refresh")
-            }
-        );
+        console.log("mounted")
+        this._refresh = this.props.navigation.addListener('focus', () => this.loadPosts());
+        // removes listener when component unmounts
+        return this._refresh;
     }
+
+    componentWillUnmount() {
+        this.props.navigation.remove
+    }
+
 
     loadPosts() {
-        this.setState({image: 'https://picsum.photos/600/400', isLoaded: true})
+        console.log("load posts")
+        api.get('/')
+            .then((response) => {
+                console.log(response.data.link)
+            })
+            .catch((e) => console.log(e))
+        this.setState({isLoading: false})
     }
-
-    // componentWillUnmount() {
-    //     this._refresh.remove();
-    // }
 
     makeChip(name) {
         return (
@@ -40,7 +56,7 @@ class Feed extends React.Component {
                 <Chip
                     icon='owl'
                     mode='outlined'
-                    style={{backgroundColor: this.props.theme.colors.accent}}
+                    style={{ backgroundColor: this.props.theme.colors.accent }}
                     onPress={() => console.log(name)}>
                     {name}
                 </Chip>
@@ -48,26 +64,52 @@ class Feed extends React.Component {
         )
     }
 
+    makeCard(post) {
+        return (
+            <Card style={{marginVertical: 10}}>
+                <Card.Title title="Post" style={{ marginLeft: 10 }} />
+                <Card.Cover source={{ uri: post.item.image }} style={{ marginVertical: 10 }} />
+                <Card.Content>
+                    <ScrollView horizontal={true}>
+                        {post.item.tags.map((name) => {
+                            return (<View
+                                key={name}
+                                style={{ margin: 5, flexWrap: 'wrap' }}>
+                                <Chip
+                                    icon='owl'
+                                    mode='outlined'
+                                    onPress={() => console.log(name)}>
+                                    {name}
+                                </Chip>
+                            </View>)
+                        })}
+                    </ScrollView>
+                </Card.Content>
+            </Card>
+        )
+    }
+
     render() {
         return (
             <View style={[style.container, { backgroundColor: this.props.theme.colors.background }]}>
                 <Header navigation={this.props.navigation} />
-                {this.state.isLoaded && <View style={style.inner}>
-                    <Card>
-                        <Card.Title title="Post" style={{marginLeft: 10}}/>
-                        <Card.Cover source={{ uri: this.state.image }} style={{ marginVertical: 10 }} />
-                        <Card.Content>
-                            <ScrollView horizontal={true}>
-                                {this.state.tags.map((name) => this.makeChip(name))}
-                            </ScrollView>
-                        </Card.Content>
-                    </Card>
-                </View>}
+                <View style={style.inner}>
+                    <FlatList
+                        data={this.state.posts}
+                        refreshing={this.state.isLoading}
+                        renderItem={this.makeCard}
+                        onRefresh={() => this.loadPosts()}
+                    />
+                </View>
                 <MediaButton navigation={this.props.navigation} />
             </View>
         );
     }
 }
+
+const api = axios.create({
+    baseURL: 'https://some-random-api.ml/img/dog',
+});
 
 const style = StyleSheet.create({
     container: {
